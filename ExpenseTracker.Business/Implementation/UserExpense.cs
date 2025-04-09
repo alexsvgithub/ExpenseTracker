e35e1f8781fc678069afb7801c298cc3367f5272
Alex Sebastian Vettithanam <alexsv9511687878@gmail.com>
2025-04-10 01:43:28 +0530

    Initial commit

---
+++ ExpenseTracker.Business/Implementation/UserExpense.cs
@@ -0,0 +1,24 @@
+﻿using ExpenseTracker.Business.Interface;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Business.Implementation
+{
+    public class UserExpense : IUserExpense
+    {
+
+        public async Task<JObject> GetAllExpenseOfUser(string userId)
+        {
+            var returnvalue = @"{'test':'abc'}";
+            await Task.Delay(1000);
+
+            return JObject.Parse(returnvalue.ToString());
+
+        }
+    }
+
+}
---
+++ ExpenseTracker.Business/Interface/IUserExpense.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Business.Interface
+{
+    public interface IUserExpense
+    {
+        public Task<JObject> GetAllExpenseOfUser(string userId);
+    }
+}
