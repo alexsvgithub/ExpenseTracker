ac7663f735278fea0d5d9d0a40ebbdfc147c8031
Alex Sebastian Vettithanam <alexsv9511687878@gmail.com>
2025-04-18 22:39:07 +0530

    Test Commit

---
+++ ExpenseTracker/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
